9d4b935f2e33ecfb6bc4457b4476b0bc6a6da078
JayaShakthi97 <jayashakthiperera@gmail.com>
2025-03-19 00:34:00 +0530

    Update global type declarations

--- app/global.d.ts
+++ app/global.d.ts
@@ -13,18 +13,14 @@ declare module "*.jpg" {
   export default content;
 }
 
-declare global {
-  interface Window {
-    config: {
-      API_BASE_URL: string;
-      API_SERVICE_URL: string;
-      APP_BASE_URL: string;
-      ASGARDEO_BASE_URL: string;
-      APP_CLIENT_ID: string;
-      DISABLED_FEATURES: string[];
-      TRANSFER_THRESHOLD: number;
-    }
+interface Window {
+  config: {
+    API_BASE_URL: string;
+    API_SERVICE_URL: string;
+    APP_BASE_URL: string;
+    ASGARDEO_BASE_URL: string;
+    APP_CLIENT_ID: string;
+    DISABLED_FEATURES: string[];
+    TRANSFER_THRESHOLD: number;
   }
 }
-
-export {};
